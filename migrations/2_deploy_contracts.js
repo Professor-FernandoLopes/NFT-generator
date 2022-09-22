6d09d0b148bb2bab3bb29913cccf554ad277c2ce
fl5050 <datacurrency@datacurrency.uk>
2022-09-21 21:20:25 -0300

    second

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,7 +9,7 @@ module.exports = async function (deployer, network) {
   //  await tokenEth.mint("1000000000000000000000", addresses[0]);
     
     await deployer.deploy(OpenPunks, "defiPunk", "DP", 0,25, 
-    1663791986
+    1665082924
 , "QmdtbSr5gHuqWPD4seobd1FmawYYcLzyJevBkCZ7TNxKno",'');
      await OpenPunks.deployed();
    // await tokenEth.transferOwnership(bridgeEth.address);
